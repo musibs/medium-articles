8f52a68c80e9d09a626249e810178066b53b05b1
musibs <musib.somnath@gmail.com>
2020-02-15 10:53:35 +1100

    Added Spring SOAP Web Service project

--- spring-soap-webservice/src/main/java/io/tbc/spring/ws/SpringSoapWebserviceApplication.java
+++ spring-soap-webservice/src/main/java/io/tbc/spring/ws/SpringSoapWebserviceApplication.java
@@ -1,13 +1,13 @@
-package io.tbc.medium.spring.ws;
+package io.tbc.spring.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWebserviceCoreApplication {
+public class SpringSoapWebserviceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringWebserviceCoreApplication.class, args);
+        SpringApplication.run(SpringSoapWebserviceApplication.class, args);
     }
 
 }
--- spring-soap-webservice/src/test/java/io/tbc/spring/ws/SpringSoapWebserviceApplicationTests.java
+++ spring-soap-webservice/src/test/java/io/tbc/spring/ws/SpringSoapWebserviceApplicationTests.java
@@ -1,10 +1,10 @@
-package io.tbc.medium.spring.ws;
+package io.tbc.spring.ws;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringWebserviceCoreApplicationTests {
+class SpringSoapWebserviceApplicationTests {
 
     @Test
     void contextLoads() {
