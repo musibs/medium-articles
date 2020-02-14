305919a6369e744a99a8ed526f7c7722567fb493
musibs <musib.somnath@gmail.com>
2020-02-15 10:25:13 +1100

    Spring Web Service demo

---
+++ spring-webservice-core/src/main/java/io/tbc/medium/spring/ws/SpringWebserviceCoreApplication.java
@@ -0,0 +1,13 @@
+package io.tbc.medium.spring.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebserviceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebserviceCoreApplication.class, args);
+    }
+
+}
---
+++ spring-webservice-core/src/test/java/io/tbc/medium/spring/ws/SpringWebserviceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tbc.medium.spring.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
