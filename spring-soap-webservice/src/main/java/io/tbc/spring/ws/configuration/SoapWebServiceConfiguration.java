ba00d1cb88edd9ddde0c0156284d241070410975
musibs <musib.somnath@gmail.com>
2020-02-15 11:16:52 +1100

    Created the WSDL and the configuration

---
+++ spring-soap-webservice/src/main/java/io/tbc/spring/ws/configuration/SoapWebServiceConfiguration.java
@@ -0,0 +1,31 @@
+package io.tbc.spring.ws.configuration;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
+
+@EnableWs
+@Configuration
+public class SoapWebServiceConfiguration extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext){
+        MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+        messageDispatcherServlet.setApplicationContext(applicationContext);
+        return new ServletRegistrationBean<>(messageDispatcherServlet, "/medium/ws/*");
+    }
+
+    @Bean(name = "calculator")
+    public Wsdl11Definition wsdl11Definition(){
+        SimpleWsdl11Definition simpleWsdl11Definition = new SimpleWsdl11Definition();
+        simpleWsdl11Definition.setWsdl(new ClassPathResource("/wsdl/calculator"));
+        return simpleWsdl11Definition;
+    }
+}
