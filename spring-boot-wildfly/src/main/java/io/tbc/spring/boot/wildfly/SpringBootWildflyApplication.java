7a7971a6b99c5098b63930c833116229e19bc9fb
musibs <musib.somnath@gmail.com>
2020-02-27 10:58:32 +1100

    Organized the modules

--- spring-boot-wildfly/src/main/java/io/tbc/spring/boot/wildfly/SpringBootWildflyApplication.java
+++ spring-boot-wildfly/src/main/java/io/tbc/spring/boot/wildfly/SpringBootWildflyApplication.java
@@ -1,14 +1,13 @@
-package io.tbc.spring.ws;
+package io.tbc.spring.boot.wildfly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSoapWebserviceApplication {
+public class SpringBootWildflyApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(SpringSoapWebserviceApplication.class, args);
+        SpringApplication.run(SpringBootWildflyApplication.class, args);
     }
 
 }
---
+++ spring-boot-wildfly/src/test/java/io/tbc/spring/boot/wildfly/SpringBootWildflyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tbc.spring.boot.wildfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWildflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
